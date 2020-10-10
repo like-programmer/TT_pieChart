9b772f7a2f2f0343a11c082640253a6251ecd977
like-programmer <dashutkadevi@gmail.com>
2020-10-10 20:38:30 +0300

    add router

--- src/components/app/app.jsx
+++ src/components/app/app.jsx
@@ -1,22 +1,31 @@
-import React, {Fragment} from "react";
+import React, {PureComponent} from "react";
+import {Switch, Route, BrowserRouter} from "react-router-dom";
 import FormPage from '../form-page/form-page.jsx';
 import GraphPage from '../graph-page/graph-page.jsx';
 
 import data from "../../mocks/userData.js";
 
 
-const App = () => {
-    return (
-        <Fragment>
-            <FormPage
-            data={data}
-            />
-            <GraphPage
-                data={data}
-            />
-        </Fragment>
-    );
-};
+class App extends PureComponent {
+    render() {
+        return (
+            <BrowserRouter>
+                <Switch>
+                    <Route exact path="/">
+                        <FormPage
+                            data={data}
+                        />
+                    </Route>
+                    <Route exact path="/graph">
+                        <GraphPage
+                            data={data}
+                        />
+                    </Route>
+                </Switch>
+            </BrowserRouter>
+        );
+    }
+}
 
 
 export default App;
